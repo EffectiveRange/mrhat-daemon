7d3903e4195969d37bf9690da8e81411f40211f6
GitHub Action <action@github.com>
2025-03-29 09:13:24 +0000

    Update version to 1.3.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class GenerateDefinitionsAndBuild(build_py):
 
 setup(
     name='mrhat-daemon',
-    version='1.2.2',
+    version='1.3.0',
     description='MrHat supervisor background service',
     long_description='MrHat supervisor background service',
     author='Ferenc Nandor Janky & Attila Gombos',
